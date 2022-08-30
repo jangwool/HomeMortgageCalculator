670c84637c21e34040e9282224c00ac622ba0518
jangwool <jangwool@vt.edu>
2022-08-29 20:50:59 -0400

    second screen + trying to add parameters to calc button

--- app/src/main/java/com/example/homemortgage/view/MainActivity.kt
+++ app/src/main/java/com/example/homemortgage/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.homemortgage.view
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.homemortgage.R
@@ -11,22 +12,67 @@ import android.content.Intent
  * James Jang
  */
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // calculate button
         val calc = findViewById<Button>(R.id.button)
 
-        val period = editText123.text.toString().toIntOrNull()
-        val rate = editText123.text.toString().toIntOrNull()
-        val amount = editText123.text.toString().toIntOrNull()
-        val principal = editText123.text.toString().toIntOrNull()
+        // input text fields
+        val period = findViewById<Button>(R.id.editTextNumber2)
+        val rate = findViewById<Button>(R.id.editTextNumber3)
+        val amount = findViewById<Button>(R.id.editTextNumber4)
+        val principal = findViewById<Button>(R.id.editTextNumber5)
 
-        val isInteger = period != null
+        val period2 = period.text.toString().toIntOrNull()
+        val rate2 = rate.text.toString().toIntOrNull()
+        val amount2 = amount.text.toString().toIntOrNull()
+        val principal2 = principal.text.toString().toIntOrNull()
 
-        calc.setOnClickListener {
-            val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
-            startActivity(intent)
+        if (period2 != null) {
+            calc.setOnClickListener {
+                val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        else {
+            // NOT A NUMBER
+            System.out.println("Please enter a valid number");
+        }
+
+        if (rate2 != null) {
+            calc.setOnClickListener {
+                val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        else {
+            // NOT A NUMBER
+            System.out.println("Please enter a valid number");
+        }
+
+        if (amount2 != null) {
+            calc.setOnClickListener {
+                val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        else {
+            // NOT A NUMBER
+            System.out.println("Please enter a valid number");
+        }
+
+        if (principal2 != null) {
+            calc.setOnClickListener {
+                val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        else {
+            // NOT A NUMBER
+            System.out.println("Please enter a valid number");
         }
     }
 }
