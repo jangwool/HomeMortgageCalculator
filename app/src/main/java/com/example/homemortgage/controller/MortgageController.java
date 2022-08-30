56c8fb63d8e681b65d10f44266886c844614d875
jangwool <jangwool@vt.edu>
2022-08-29 20:25:15 -0400

    second screen + trying to add parameters to calc button

--- app/src/main/java/com/example/homemortgage/controller/MortgageController.java
+++ app/src/main/java/com/example/homemortgage/controller/MortgageController.java
@@ -1,6 +1,17 @@
 package com.example.homemortgage.controller;
 
 public class MortgageController {
+    private int period;
+    private int rate;
+    private int amount;
+    private int principal;
+
+    public MortgageController() {
+        this.period = period;
+        this.rate = rate;
+        this.amount = amount;
+        this.principal = principal;
+    }
 
 
 }
--- app/src/main/java/com/example/homemortgage/view/MainActivity.kt
+++ app/src/main/java/com/example/homemortgage/view/MainActivity.kt
@@ -3,16 +3,30 @@ package com.example.homemortgage.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.homemortgage.R
+import android.widget.Button
+import android.content.Intent
 
 /**
  * Inti Espejo
  * James Jang
- *
- *
  */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calc = findViewById<Button>(R.id.button)
+
+        val period = editText123.text.toString().toIntOrNull()
+        val rate = editText123.text.toString().toIntOrNull()
+        val amount = editText123.text.toString().toIntOrNull()
+        val principal = editText123.text.toString().toIntOrNull()
+
+        val isInteger = period != null
+
+        calc.setOnClickListener {
+            val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
