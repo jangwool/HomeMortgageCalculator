a627754af3974be71e1e5eb0f6c32b70b4684b79
jangwool <jangwool@vt.edu>
2022-09-02 16:43:39 -0400

    added back button to second screen + working on clickable button after inputs

--- app/src/main/java/com/example/homemortgage/SecondActivity.kt
+++ app/src/main/java/com/example/homemortgage/SecondActivity.kt
@@ -1,11 +1,34 @@
 package com.example.homemortgage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        var bar = getSupportActionBar()
+
+        // shows back button
+        if (bar != null) {
+            bar.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    // makes back button go back to first screen
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/homemortgage/view/MainActivity.kt
+++ app/src/main/java/com/example/homemortgage/view/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.homemortgage.view
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.homemortgage.R
-import android.widget.Button
 import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.homemortgage.R
+import android.text.TextWatcher
+import android.text.Editable
+
 
 /**
  * Inti Espejo
  * James Jang
  */
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,59 +23,56 @@ class MainActivity : AppCompatActivity() {
         // calculate button
         val calc = findViewById<Button>(R.id.button)
 
+        calc.setEnabled(false)
+        calc.setClickable(false)
+
         // input text fields
-        val period = findViewById<Button>(R.id.editTextNumber2)
-        val rate = findViewById<Button>(R.id.editTextNumber3)
-        val amount = findViewById<Button>(R.id.editTextNumber4)
-        val principal = findViewById<Button>(R.id.editTextNumber5)
+        val period = findViewById<EditText>(R.id.editTextNumber2)
+        val rate = findViewById<EditText>(R.id.editTextNumber3)
+        val amount = findViewById<EditText>(R.id.editTextNumber4)
+        val principal = findViewById<EditText>(R.id.editTextNumber5)
 
-        val period2 = period.text.toString().toIntOrNull()
-        val rate2 = rate.text.toString().toIntOrNull()
-        val amount2 = amount.text.toString().toIntOrNull()
-        val principal2 = principal.text.toString().toIntOrNull()
+        //period.getText().toString().isEmpty()
+        //TextUtils.isEmpty(period.getText().toString())
 
-        if (period2 != null) {
+        //Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_SHORT).show()
+
+        period.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        if (period.getText().toString() == "") {
+            calc.setEnabled(false)
+            calc.setClickable(false)
+        }
+        else if (rate.getText().toString() == "") {
+            calc.setEnabled(false)
+            calc.setClickable(false)
+        }
+        else if (amount.getText().toString() == "") {
+            calc.setEnabled(false)
+            calc.setClickable(false)
+        }
+        else if (principal.getText().toString() == "") {
+            calc.setEnabled(false)
+            calc.setClickable(false)
+        }
+        else {
+            // All number fields have been entered
+            calc.setEnabled(true)
+            calc.setClickable(true)
+
             calc.setOnClickListener {
                 val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
                 startActivity(intent)
             }
-        }
-        else {
-            // NOT A NUMBER
-            System.out.println("Please enter a valid number");
-        }
-
-        if (rate2 != null) {
-            calc.setOnClickListener {
-                val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
-                startActivity(intent)
-            }
-        }
-        else {
-            // NOT A NUMBER
-            System.out.println("Please enter a valid number");
-        }
-
-        if (amount2 != null) {
-            calc.setOnClickListener {
-                val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
-                startActivity(intent)
-            }
-        }
-        else {
-            // NOT A NUMBER
-            System.out.println("Please enter a valid number");
-        }
-
-        if (principal2 != null) {
-            calc.setOnClickListener {
-                val intent = Intent(this, com.example.homemortgage.SecondActivity::class.java)
-                startActivity(intent)
-            }
-        }
-        else {
-            // NOT A NUMBER
-            System.out.println("Please enter a valid number");
         }
     }
 }
